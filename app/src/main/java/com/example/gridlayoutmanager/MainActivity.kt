073f0442b99804a28a96fbf2e8a513ee3a9bc34f
Denis Latushko <denis.latushko@cogniteq.com>
2023-10-15 15:25:09 +0200

    Implemented main activity layout with recycler view

--- app/src/main/java/com/example/gridlayoutmanager/MainActivity.kt
+++ app/src/main/java/com/example/gridlayoutmanager/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.gridlayoutmanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.gridlayoutmanager.data.DataProvider
+import com.example.gridlayoutmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initList()
+    }
+
+    private fun initList() {
+        binding.viewItemsList.apply {
+            layoutManager = GridLayoutManager(this@MainActivity, 5, GridLayoutManager.HORIZONTAL, false)
+            adapter = ItemsAdapter().also { it.submitList(DataProvider.buildData()) }
+        }
     }
 }
